a22643535eb15913b862e5d82f7424915df1f6b8
Robert Chamisa-Denhere <chamisa.robert@gmail.com>
2020-06-30 17:36:04 +0200

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '777955716280392',
+        clientSecret: '734038410306d44f9321633d04178de0'
+    }
 }
